f224fe15aefd4651f8bf96a49ba99082e7d1e242
hxx258456 <13011715880@163.com>
2023-07-20 22:41:03 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tsinghua_rust_lab
 清华rust lab
+
+[程序设计训练（Rust） (tsinghua.edu.cn)](https://lab.cs.tsinghua.edu.cn/rust/)
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please enter a number.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small!"),
+            Ordering::Greater => println!("To big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
