9174b2fa145cb37d965900551f0d0d6eebdd7766
hxx258456 <13011715880@163.com>
2023-07-21 14:29:33 +0800

    feat: guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,31 @@
+#![feature(core_intrinsics)]
 use rand::Rng;
 use std::{cmp::Ordering, io};
+
+#[warn(unsafe_code)]
+fn print_type_of<T>(_: &T) {
+    println!("{}", unsafe { std::intrinsics::type_name::<T>() });
+}
 
 fn main() {
     println!("Guess the number!");
     println!("Please enter a number.");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
+
     loop {
         let mut guess = String::new();
+        print_type_of(&guess); // <<--------为什么这里变成guess String类型
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(a) => {
+                println!("{a}");
+                continue;
+            }
         };
         println!("You guessed: {guess}");
         match guess.cmp(&secret_number) {
@@ -21,8 +33,10 @@ fn main() {
             Ordering::Greater => println!("To big!"),
             Ordering::Equal => {
                 println!("You win!");
+
                 break;
             }
         }
+        print_type_of(&guess); // <<--------这里guess u32类型
     }
 }
